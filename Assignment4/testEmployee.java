38a6f5f01168a6524ce2eab417ef643d2caa0b9c
Jay Patel <65984709+Patel1198@users.noreply.github.com>
2021-08-24 15:54:04 -0400

    test salary of employee

--- Assignment4/testEmployee.java
+++ Assignment4/testEmployee.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 // this is test class to test the employee and employeeException class.
 public class testEmployee {
     public static void main(String[] args) {
-        /*
+        /* 
             this try-catch blocks will take the id number and hourlyWage and
             catch an exception if the value of the hourlyWage is less than 6.00
             or greater than 50.00.
